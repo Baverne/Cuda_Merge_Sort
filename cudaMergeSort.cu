e518695b4b5fcfe408624f6fb59704d53fd5d63c
gaelthw <gael.thieuw@gmail.com>
2026-03-12 19:58:21 +0100

    Exercice3

--- cudaMergeSort.cu
+++ cudaMergeSort.cu
@@ -153,6 +153,11 @@ __global__ void mergeSmallBatch_k(float* ABs, float* Ms, int* cardAs, int N, int
 }
 
 __global__ void sortSmallBatch_k(float* Ms, int N, int d) {
+    /* Sorts each array in Ms
+    - Ms: contains the N arrays of length d, stored contiguously
+    - N: The number of arrays to sort
+    - d: The length of each array, must be <= 1024 due to the fact that max 1024 threads can be launched by a single block 
+    */
 
     int list_idx = blockIdx.x; // The list processed by this thread will be Ms[list_idx * d : list_idx * d + d]
     if (list_idx >= N) return; // This block is useless
@@ -251,11 +256,78 @@ __global__ void sortSmallBatch_k(float* Ms, int N, int d) {
 
 }
 
+__global__ void mergeBatch_k(float* ABs, float* Ms, int* cardAs, int N, int d) {
+    int gbx = threadIdx.x + blockDim.x * blockIdx.x;
+    int list_idx = gbx / d;
+    if (list_idx >= N) {
+        return;
+    }
+    float* A = &ABs[list_idx * d];
+    int cardA = cardAs[list_idx];
+    int cardB = d - cardA;
+    float* B = &ABs[list_idx * d + cardA];
+    float* M = &Ms[list_idx * d];
+    int tidx = gbx - d * list_idx;
+
+    int Kx, Ky, Px, Py, Qx, Qy;
+    int offset;
+    
+
+    if (tidx > cardA) {
+        Kx = tidx-cardA;
+        Ky = cardA;
+        Px = cardA;
+        Py = tidx-cardA;
+    }
+    else {
+        // In this case we initialize the K and P to the frontier of the diagonal
+        Kx = 0;
+        Ky = tidx;
+        Px = tidx;
+        Py = 0;
+    }
+    while (true) {
+        // We proceed by dichotomy, offset is therefore the half of the distance between the two frontiers
+        offset = abs(Ky-Py)/2;
+        Qx = Kx + offset;
+        Qy = Ky - offset;
+
+        
+        if (Qy >= 0 && Qx <= cardB && (Qy == cardA || Qx == 0 || A[Qy] > B[Qx-1])) {
+            if (Qx == cardB || Qy == 0 || A[Qy-1] <= B[Qx]) {
+                // We cornered the solution, we can return the value of M[i]
+                if (Qy < cardA && (Qx == cardB || A[Qy] <= B[Qx])) {
+                    M[tidx] = A[Qy];
+                }
+                else {
+                    M[tidx] = B[Qx];
+                }
+                break;
+            }
+            else {
+                // In this case our solution is in the upper right part of the diagonal
+                Kx = Qx + 1;
+                Ky = Qy - 1;
+            }
+        }
+        else {
+            // In this case our solution is in the lower left part of the diagonal
+            Px = Qx - 1;
+            Py = Qy + 1;
+        }
+        
+
+    }
+
+
+}
+
 int main(void) {
     //*********************//
     //// TEST EXERCICE 1 ////
     //*********************//
     // Two sorted input arrays
+
     int cardA = 10, cardB = 13;
     int cardM = cardA + cardB;
 
@@ -301,9 +373,9 @@ int main(void) {
     cudaFree(d_B);
     cudaFree(d_M);
 
-    //*********************//
-    //// TEST EXERCICE 2 ////
-    //*********************//
+    //***********************//
+    //// TEST EXERCICE 2.a ////
+    //***********************//
     {
         const int N = 100000;
         int d_vals[] = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
@@ -450,6 +522,117 @@ int main(void) {
         fclose(csv);
         printf("Benchmark results saved to benchmark2b.csv\n");
         
+    }
+
+    //*********************//
+    //// TEST EXERCICE 3 ////
+    //*********************//
+    {
+        const int N = 100000;
+        int d_vals[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
+        int num_d = 14;
+
+        FILE* csv = fopen("benchmark3.csv", "w");
+        fprintf(csv, "d,N,time_ms\n");
+
+        for (int di = 0; di < num_d; di++) {
+            // for each d, we will measure sorting time
+            int d = d_vals[di];
+            int cardA_val = d / 2; // longest merge is when both list are equal size
+            int cardB_val = d - cardA_val;
+
+            // Build host ABs: each pair i = sorted A (cardA_val floats) then sorted B (cardB_val floats)
+            float* h_ABs   = (float*)malloc(N * d * sizeof(float));
+            int*   h_cardAs = (int*)malloc(N * sizeof(int));
+            
+            // In order to build sorted list of a given size, we add a random value between 0 and 9 to the previous one
+            for (int i = 0; i < N; i++) {
+                h_cardAs[i] = cardA_val;
+                float val = 0.0f;
+                for (int j = 0; j < cardA_val; j++) {
+                    val += (float)(rand() % 10);
+                    h_ABs[i * d + j] = val;
+                }
+                val = 0.0f;
+                for (int j = 0; j < cardB_val; j++) {
+                    val += (float)(rand() % 10);
+                    h_ABs[i * d + cardA_val + j] = val;
+                }
+            }
+
+            // printf("Lists before merging:\n");
+            // for (int i = 0; i < N; i++){
+            //     printf("A: ");
+            //     for (int j = 0 ; j < h_cardAs[i] ; j++) {
+            //         printf("%.0f ", h_ABs[i * d + j]);
+            //     }
+            //     printf("\nB: ");
+            //     for (int j = h_cardAs[i] ; j < d ; j++) {
+            //         printf("%.0f ", h_ABs[i * d + j]);
+            //     }
+            //     printf("\n");
+            // }
+
+            float* d_ABs;
+            float* d_Ms;
+            int*   d_cardAs;
+            testCUDA(cudaMalloc((void**)&d_ABs,    N * d * sizeof(float)));
+            testCUDA(cudaMalloc((void**)&d_Ms,     N * d * sizeof(float)));
+            testCUDA(cudaMalloc((void**)&d_cardAs, N * sizeof(int)));
+            testCUDA(cudaMemcpy(d_ABs,    h_ABs,    N * d * sizeof(float), cudaMemcpyHostToDevice));
+            testCUDA(cudaMemcpy(d_cardAs, h_cardAs, N * sizeof(int),       cudaMemcpyHostToDevice));
+
+            int num_blocks = 1 + N * d / 1024;
+
+            // Warmup
+            mergeBatch_k<<<num_blocks, 1024>>>(d_ABs, d_Ms, d_cardAs, N, d);
+            testCUDA(cudaDeviceSynchronize());
+
+            // Timed run
+            cudaEvent_t start, stop;
+            float elapsed_ms = 0.0f;
+            testCUDA(cudaEventCreate(&start));
+            testCUDA(cudaEventCreate(&stop));
+            testCUDA(cudaEventRecord(start));
+            mergeBatch_k<<<num_blocks, 1024>>>(d_ABs, d_Ms, d_cardAs, N, d);
+            testCUDA(cudaEventRecord(stop));
+            testCUDA(cudaEventSynchronize(stop));
+            testCUDA(cudaEventElapsedTime(&elapsed_ms, start, stop));
+            
+            float* h_Ms   = (float*)malloc(N * d * sizeof(float));
+            testCUDA(cudaMemcpy(h_Ms,    d_Ms,    N * d * sizeof(float), cudaMemcpyDeviceToHost));
+
+            // printf("Lists after merging:\n");
+            // for (int i = 0; i < N ; i++){
+            //     for (int j = 0 ; j < d ; j++){
+            //         printf("%.0f ", h_Ms[i * d + j]);
+            //     }
+            //     printf("\n");
+            // }
+
+            // for (int i = 0 ; i < d * N ; i++) {
+            //     if (i % d == 0) continue;
+            //     if (h_Ms[i-1] > h_Ms[i]) {
+            //         printf("The list is not sorted\n");
+            //     }
+            // }
+
+            printf("d=%4d | N=%d | time=%.4f ms\n", d, N, elapsed_ms);
+            fprintf(csv, "%d,%d,%.6f\n", d, N, elapsed_ms);
+
+            testCUDA(cudaEventDestroy(start));
+            testCUDA(cudaEventDestroy(stop));
+            cudaFree(d_ABs);
+            cudaFree(d_Ms);
+            cudaFree(d_cardAs);
+            free(h_ABs);
+            free(h_cardAs);
+        }
+        fclose(csv);
+        printf("Benchmark results saved to benchmark3.csv\n");
+
+        
+
     }
 
 	return 0;
